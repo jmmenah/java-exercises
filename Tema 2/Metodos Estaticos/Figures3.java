28012087f8dd2ad2c1dab56713c4a5724a53999d
jmmenah <55758232+jmmenah@users.noreply.github.com>
2019-10-04 11:58:51 +0200

    Add files via upload

---
+++ Tema 2/Metodos Estaticos/Figures3.java
@@ -0,0 +1,60 @@
+// Building Java Programs: A Back to Basics Approach, 4th edition 
+// by Stuart Reges and Marty Stepp
+// http://www.buildingjavaprograms.com/
+
+// Suzy Student, CSE 138, Spring 2094
+// Prints several figures, with methods for structure and redundancy.
+public class Figures3 {
+    public static void main(String[] args) {
+        egg();
+        teaCup();
+        stopSign();
+        hat();
+    }
+    
+    // Draws the top half of an an egg figure.
+    public static void eggTop() {
+        System.out.println("  ______");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+    }
+    
+    // Draws the bottom half of an egg figure.
+    public static void eggBottom() {
+        System.out.println("\\        /");
+        System.out.println(" \\______/");
+    }
+    
+    // Draws a complete egg figure.
+    public static void egg() {
+        eggTop();
+        eggBottom();
+        System.out.println();
+    }
+
+ // Draws a teacup figure.
+    public static void teaCup() {
+        eggBottom();
+        line();
+        System.out.println();
+    }
+    
+    // Draws a stop sign figure.
+    public static void stopSign() {
+        eggTop();
+        System.out.println("|  STOP  |");
+        eggBottom();
+        System.out.println();
+    }
+    
+    // Draws a figure that looks sort of like a hat.
+    public static void hat() {
+        eggTop();
+        line();
+    }
+
+    // Draws a line of dashes.
+    public static void line() {
+        System.out.println("+--------+");
+    }
+}    
