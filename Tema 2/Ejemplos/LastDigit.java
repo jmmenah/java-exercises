5d4fafb3bdb4ea4c9d8148dae8db8c6c00cd3215
jmmenah <55758232+jmmenah@users.noreply.github.com>
2019-10-04 12:11:29 +0200

    Delete LastDigit.java

--- Tema 2/Ejemplos/LastDigit.java
+++
@@ -1,8 +0,0 @@
-public class LastDigit { 
-    public static void main(String[] args) { 
-        // a positive integer
-        int n = Integer.parseInt(args[0]);
-	System.out.println(n % 10);
-    }
-}
-
