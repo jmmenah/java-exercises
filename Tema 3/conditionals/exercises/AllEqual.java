88871df7396f4c0872dc4e4b9c66c60b18e3763d
jmmenah <55758232+jmmenah@users.noreply.github.com>
2019-10-16 12:27:28 +0200

    Add files via upload

---
+++ Tema 3/conditionals/exercises/AllEqual.java
@@ -0,0 +1,16 @@
+public class AllEqual { 
+    public static void main(String[] args) { 
+       	int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+		if (a == b & b == c)
+		{
+		System.out.println("They are equals");
+		}
+		else
+		{
+		System.out.println("They are not equals");
+		}
+	}
+}
+	
---
+++ Tema 3/conditionals/exercises/DistinctValues.java
@@ -0,0 +1,19 @@
+public class DistinctValues { 
+	public static void main(String[] args) { 
+       		int a = Integer.parseInt(args[0]);
+        	int b = Integer.parseInt(args[1]);
+        	int c = Integer.parseInt(args[2]);
+			if (a == b & b == c)
+			{
+			System.out.println("There is only 1 distinct value.");
+			}
+			else if (a == b || b == c || a == c)
+			{
+			System.out.println("There are 2 distinct values.");
+			}
+			else
+			{
+			System.out.println("There are 3 distinct values.");
+			}
+	}
+}
