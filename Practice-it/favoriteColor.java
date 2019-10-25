8e5b15054dea8d8abc5fa1c394fd6e4374a8a501
jmmenah <55758232+jmmenah@users.noreply.github.com>
2019-10-25 14:16:09 +0200

    Create favoriteColor.java

---
+++ Practice-it/favoriteColor.java
@@ -0,0 +1,6 @@
+Scanner console = new Scanner(System.in);
+System.out.print("What is your favorite color? ");
+String name = console.next();
+if (name.equals("blue")) {
+    System.out.println("Mine, too!");
+}
