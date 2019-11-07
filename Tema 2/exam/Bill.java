5d6697977c42b91a4961636a3dfa15539f3ab9c0
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2019-11-07 21:55:33 +0100

    Update Bill.java

--- Tema 2/exam/Bill.java
+++ Tema 2/exam/Bill.java
@@ -1,8 +1,8 @@
 public class Bill{
 	public static void main(String[]args){
-		double value = Double.parseDouble(args[0]);
+		double value = Double.parseDouble(args[0]);//int value = Integer.parseInt(args[0]);
 		int iva = Integer.parseInt(args[1]);
-		double totalValue = value + (value*iva/100);
+		double totalValue = value + (value*iva/100); //double totalValue = (double)value + (value*iva/100);
 		System.out.println("Bill = " + value +"€ + " + iva +"% IVA = " + totalValue + "€" );
 	}
 }
