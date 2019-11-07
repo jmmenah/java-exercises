6a581648ff8dc84a8643fbd6459e127191ae0f32
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2019-11-07 21:56:44 +0100

    Update Bill.java

--- Tema 2/exam/Bill.java
+++ Tema 2/exam/Bill.java
@@ -1,3 +1,20 @@
+/*
+2.- Write a program Bill.java that takes two integer command-line arguments (money and iva) and prints the bill identical as shown below. (4 points)
+
+Examples of execution:
+
+$ java Bill 100 21
+Bill = 100€ + 21% IVA = 121.0€
+
+$ java Bill 100 10
+Bill = 100€ + 10% IVA = 110.0€
+
+$ java Bill 123 4
+Bill = 123€ + 4% IVA = 127.92€
+
+$ java Bill 896 21
+Bill = 896€ + 21% IVA = 1084.16€
+*/
 public class Bill{
 	public static void main(String[]args){
 		double value = Double.parseDouble(args[0]);//int value = Integer.parseInt(args[0]);
