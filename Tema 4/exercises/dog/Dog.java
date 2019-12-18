df5d30ea119abfd18183fd2b61a4b2a3024209d9
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2019-12-18 18:31:58 +0100

    Add files via upload

---
+++ Tema 4/exercises/dog/Dog.java
@@ -0,0 +1,39 @@
+public class Dog{
+	private String name;
+	private int age;
+
+	
+	public Dog(String name, int age){
+		this.name=name;
+		this.age=age;
+	}
+
+
+	public void setName(String name){
+		this.name=name;
+	}
+
+	public void setAge(int age){
+		this.age=age;
+	}
+
+
+	public String getName(){
+		return name;
+	}
+
+	public int getAge(){
+		return age;
+	}
+
+
+	public int dogPersonYears(){
+		return age*7;
+	}
+
+
+	public String toString(){
+		return "Dog [age="+age+",name="+name+"]";
+	}
+
+}
---
+++ Tema 4/exercises/dog/DogTest.java
@@ -0,0 +1,31 @@
+public class DogTest{
+	public static void main(String[] args) {
+		Dog[] dogs= new Dog[4];
+		dogs[0]= new Dog("Milú",2);
+		dogs[1]= new Dog("Snoopy",1);
+		dogs[2]= new Dog("Idéfix",3);
+		dogs[3]= new Dog("Pluto",5);
+
+		System.out.println();
+		System.out.println("Calling showDogs");
+		System.out.println();
+		showDogs(dogs);
+		System.out.println();
+		System.out.println("Calling showDogPersonYears");
+		System.out.println();
+		showDogPersonYears(dogs);
+		System.out.println();
+	}
+
+	public static void showDogs(Dog[] dogs){
+		for(int i=0;i<dogs.length;i++){
+			System.out.println("Dog #"+(i+1)+": "+dogs[i]);
+		}
+	}
+
+	public static void showDogPersonYears(Dog[] dogs){
+		for(Dog dog : dogs){
+			System.out.println(dog.getName()+": "+dog.dogPersonYears()+" person years");
+		}
+	}
+}
