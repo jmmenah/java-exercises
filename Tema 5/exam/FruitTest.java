e1ff8df87083e1ff9edfe5b29476dcce87205ffe
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2020-01-30 22:07:28 +0100

    Update FruitTest.java

--- Tema 5/exam/FruitTest.java
+++ Tema 5/exam/FruitTest.java
@@ -1,3 +1,44 @@
+/*
+Code the next two programs and upload your Java source code.
+
+1.- Complete the program FruitTest.java  that uses Fruit class Fruit.java. (7 points)
+
+    Follow the instructions shown in the comments. 
+    You are NOT allowed to modify the Fruit class except for:
+        adding a toString method and
+        complete the code of the method startsWithVowel
+
+The output of the program must be identical to the one shown (pay attention to lower and upper case):
+
+$ java FruitTest
+ 
+Show List of Fruits:
+Fruit #1: An Apple with "green" color
+Fruit #2: An Apple with "red" color
+Fruit #3: An Apple with "yellow" color
+Fruit #4: A Banana with "yellow" color
+Fruit #5: An Orange with "orange" color
+Fruit #6: A Cherry with "red" color
+Fruit #7: A Grape with "purple" color
+Fruit #8: A Grape with "green" color
+Fruit #9: A Peach with "yellow" color
+Fruit #10: A Peach with "red" color
+
+ RED fruits:
+An Apple with "red" color
+A Cherry with "red" color
+A Peach with "red" color
+Total = 3
+
+ BROWN fruits:
+Total = 0
+
+ GREEN fruits:
+An Apple with "green" color
+A Grape with "green" color
+Total = 2
+*/
+
 import java.util.ArrayList;
 
 public class FruitTest {
