9ce44b180e8d4ebad110228f353f31658baf6859
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2020-04-21 18:32:18 +0200

    Add files via upload

---
+++ Tema 9/examples/TicTacToe/src/e02/GameState.java
@@ -0,0 +1,8 @@
+package e02;
+
+/**
+ *  Enumerations for the various states of the game
+ */
+public enum GameState {  // to save as "GameState.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
---
+++ Tema 9/examples/TicTacToe/src/e02/Seed.java
@@ -0,0 +1,8 @@
+package e02;
+
+/**
+ * Enumerations for the seeds and cell contents
+ */
+public enum Seed { // to save as "Seed.java"
+    EMPTY, CROSS, NOUGHT
+}
