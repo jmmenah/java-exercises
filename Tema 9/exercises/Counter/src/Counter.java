065c0e826ac00cd2bd65b3df7fab7708914e1959
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2020-05-01 21:05:12 +0200

    Add files via upload

---
+++ Tema 9/exercises/Counter/src/Counter.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+public class Counter extends Application {
+    private TextField tfCount;
+    private Button btnCount;
+    private Button rstButton;
+    private int count = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Allocate controls
+        tfCount = new TextField("0");
+        tfCount.setEditable(false);
+        btnCount = new Button("Count");
+        // Register event handler using Lambda Expression (JDK 8)
+        btnCount.setOnAction(evt -> tfCount.setText(++count + ""));
+        rstButton = new Button("Reset");
+        rstButton.setOnAction(evt -> {tfCount.setText("0");
+                                      count=0;});
+
+
+        // Create a scene graph of node rooted at a FlowPane
+        FlowPane flow = new FlowPane();
+        flow.setPadding(new Insets(15, 12, 15, 12));  // top, right, bottom, left
+        flow.setVgap(10);  // Vertical gap between nodes in pixels
+        flow.setHgap(10);  // Horizontal gap between nodes in pixels
+        flow.setAlignment(Pos.CENTER);  // Alignment
+        flow.getChildren().addAll(new Label("Count: "), tfCount, btnCount,rstButton);
+
+        // Setup scene and stage
+        primaryStage.setScene(new Scene(flow, 400, 80));
+        primaryStage.setTitle("JavaFX Counter");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Tema 9/exercises/FXGame/src/com/mkyong/javafx/animatedball/MyFirstGame2.java
@@ -0,0 +1,92 @@
+package com.mkyong.javafx.animatedball;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Bounds;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+public class MyFirstGame2 extends Application{
+
+    private int count = 0;
+
+    @Override
+    public void start(Stage stage) {
+
+        Pane canvas = new Pane();
+        Scene scene = new Scene(canvas, 300, 300, Color.ALICEBLUE);
+        Circle ball = new Circle(10, Color.DARKSLATEBLUE);
+        ball.relocate(5, 5);
+        Label counter= new Label("Score: "+count);
+
+        canvas.getChildren().addAll(ball,counter);
+
+        stage.initStyle(StageStyle.DECORATED);
+
+        ball.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                    count++;
+                counter.setText("Score: "+count);
+                if (count==5){
+                    Platform.exit();
+                    System.exit(0);
+                }
+            }
+        });
+
+        stage.setTitle("Animated Ball");
+        stage.setScene(scene);
+        stage.show();
+
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(20), new EventHandler<ActionEvent>() {
+            double dx = 2; //Step on x or velocity
+            double dy = 3; //Step on y
+
+            @Override
+            public void handle(ActionEvent t) {
+                //move the ball
+                ball.setLayoutX(ball.getLayoutX() + dx);
+                ball.setLayoutY(ball.getLayoutY() + dy);
+
+                Bounds bounds = canvas.getBoundsInLocal();
+
+                //If the ball reaches the left or right border make the step negative
+                if(ball.getLayoutX() <= (bounds.getMinX() + ball.getRadius()) ||
+                        ball.getLayoutX() >= (bounds.getMaxX() - ball.getRadius()) ){
+
+                    dx = -dx;
+
+                }
+
+                //If the ball reaches the bottom or top border make the step negative
+                if((ball.getLayoutY() >= (bounds.getMaxY() - ball.getRadius())) ||
+                        (ball.getLayoutY() <= (bounds.getMinY() + ball.getRadius()))){
+
+                    dy = -dy;
+
+                }
+
+            }
+        }));
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
